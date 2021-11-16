8d45094981891c937bf7b52f03ef385864ba2935
arfan21 <arfan.jadul10@gmail.com>
2021-11-16 22:00:47 +0700

    add entity & mode socialmedia, add repo socialmedia with method create

---
+++ model/modelsocialmedia/socialmedia_web.go
@@ -0,0 +1,31 @@
+package modelsocialmedia
+
+import "time"
+
+type Request struct {
+	Name           string `json:"name"`
+	SocialMediaUrl string `json:"social_media_url"`
+}
+
+type Response struct {
+	ID             uint      `json:"id"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
+	UpdatedAt      time.Time `json:"updated_at,omitempty"`
+	Name           string    `json:"name"`
+	SocialMediaUrl string    `json:"social_media_url"`
+	UserID         uint      `json:"user_id"`
+}
+
+type ResponseList struct {
+	ID             uint      `json:"id"`
+	CreatedAt      time.Time `json:"created_at,omitempty"`
+	UpdatedAt      time.Time `json:"updated_at,omitempty"`
+	Name           string    `json:"name"`
+	SocialMediaUrl string    `json:"social_media_url"`
+	UserID         uint      `json:"user_id"`
+	User           struct {
+		ID              uint   `json:"id"  example:"1"`
+		Username        string `json:"username"  example:"jhondoe"`
+		ProfileImageUrl string `json:"profile_image_url" example:"https://example.com/photo.jpg"`
+	} `json:"user"`
+}
