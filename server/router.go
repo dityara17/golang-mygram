f575aeb1876cc66f4e424620b069ecdeaeae43e3
arfan21 <arfan.jadul10@gmail.com>
2021-11-28 19:27:56 +0700

    add controller socialmedia

--- server/router.go
+++ server/router.go
@@ -3,14 +3,17 @@ package server
 import (
 	"github.com/arfan21/golang-mygram/controller/controllercomment"
 	"github.com/arfan21/golang-mygram/controller/controllerphoto"
+	"github.com/arfan21/golang-mygram/controller/controllersocialmedia"
 	"github.com/arfan21/golang-mygram/controller/controlleruser"
 	_ "github.com/arfan21/golang-mygram/docs"
 	"github.com/arfan21/golang-mygram/middleware"
 	"github.com/arfan21/golang-mygram/repository/repositorycomment"
 	"github.com/arfan21/golang-mygram/repository/repositoryphoto"
+	"github.com/arfan21/golang-mygram/repository/repositorysocialmedia"
 	"github.com/arfan21/golang-mygram/repository/repositoryuser"
 	"github.com/arfan21/golang-mygram/service/servicecomment"
 	"github.com/arfan21/golang-mygram/service/servicephoto"
+	"github.com/arfan21/golang-mygram/service/servicesocialmedia"
 	"github.com/arfan21/golang-mygram/service/serviceuser"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
@@ -26,6 +29,7 @@ func NewRouter(r *gin.Engine, db *gorm.DB) {
 
 	routeUser := r.Group("/users")
 
+	// route user
 	routeUser.POST("/register", ctrlUser.Create)
 	routeUser.POST("/login", ctrlUser.Login)
 	routeUser.PUT("", middleware.Authorization, ctrlUser.Update)
@@ -36,7 +40,7 @@ func NewRouter(r *gin.Engine, db *gorm.DB) {
 	srvPhoto := servicephoto.New(repoPhoto)
 	ctrlPhoto := controllerphoto.New(srvPhoto)
 
-	r.GET("photos", middleware.Authorization, ctrlPhoto.GetByUserID)
+	r.GET("photos", middleware.Authorization, ctrlPhoto.GetPhotos)
 	r.POST("photos", middleware.Authorization, ctrlPhoto.Create)
 	r.PUT("photos/:photoID", middleware.Authorization, ctrlPhoto.Update)
 	r.DELETE("photos/:photoID", middleware.Authorization, ctrlPhoto.Delete)
@@ -49,6 +53,16 @@ func NewRouter(r *gin.Engine, db *gorm.DB) {
 	r.POST("comments", middleware.Authorization, ctrlComment.Create)
 	r.PUT("comments/:commentID", middleware.Authorization, ctrlComment.Update)
 	r.DELETE("comments/:commentID", middleware.Authorization, ctrlComment.Delete)
+
+	// route social media
+	repoSocialmedia := repositorysocialmedia.New(db)
+	srvSocialmedia := servicesocialmedia.New(repoSocialmedia, repoPhoto)
+	ctrlSocialmedia := controllersocialmedia.New(srvSocialmedia)
+	routerSocialmedia := r.Group("/socialmedias")
+	routerSocialmedia.POST("", middleware.Authorization, ctrlSocialmedia.Create)
+	routerSocialmedia.GET("", middleware.Authorization, ctrlSocialmedia.GetList)
+	routerSocialmedia.PUT("/:socialmediaid", middleware.Authorization, ctrlSocialmedia.UpdateByID)
+	routerSocialmedia.DELETE("/:socialmediaid", middleware.Authorization, ctrlSocialmedia.DeleteByID)
 
 	// routing docs
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
